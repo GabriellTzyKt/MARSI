98db40bdf1b1f3f3fc0490c03c724b46a982bdf7
Fortibo <pootisspy931@gmail.com>
2025-02-06 16:56:13 +0700

    add: website umum landing

---
+++ fe-web/src/routes/umum/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../../app.css';
+    import { page } from '$app/state';
+	import Navbar from './nav/Navbar.svelte';
+	let { children } = $props();
+</script>
+
+<div>
+    <Navbar></Navbar>
+</div>
+<div>
+    {@render children()}    
+</div>
