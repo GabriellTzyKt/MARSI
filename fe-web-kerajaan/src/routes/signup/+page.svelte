c241339806f66db9cd156178e039cff407587507
leodam17 <c14220172@john.petra.ac.id>
2025-02-13 16:49:10 +0700

    temp kerajaan

--- fe-web-kerajaan/src/routes/signup/+page.svelte
+++ fe-web-kerajaan/src/routes/signup/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import gambar from '$lib/asset/kerajaan/image.svg';
+	import gambar2 from '$lib/asset/kerajaan/temp_gbr_login.png';
 </script>
 
 <!-- Navbar -->
@@ -7,13 +8,34 @@
 
 <!-- Singup Page -->
 <section>
-	<div class="relative">
-		<img src={gambar} alt="" class="h-full w-full" />
-		<div class="absolute top-0 left-0 flex items-center justify-center h-full w-full">
-			<p class="h-[550px] w-[450px] rounded-lg border border-gray-500 bg-white opacity-75"></p>
-		</div>
-	</div>
+    <div class="relative">
+        <img src={gambar} alt="" class="w-full h-auto min-h-screen" />
+        <div class="absolute top-0 left-0 flex h-full w-full items-center justify-center p-4">
+            <div class="h-fit w-full max-w-[450px] rounded-lg border border-gray-500 bg-gray-200 opacity-90 p-6">
+                <div class="mt-6 flex w-full justify-center">
+                    <img src={gambar2} alt="" class="h-[25%] w-[25%]" />
+                </div>
+                <div class="flex flex-col mt-4">
+                    <label for="username" class="text-md">Username</label>
+                    <input type="text" id="username" class="mt-2 w-full p-2 bg-white rounded-md border border-black" />
+                </div>
+                <div class="flex flex-col mt-4">
+                    <label for="password" class="text-md">Kata Sandi</label>
+                    <input type="password" id="password" class="mt-2 w-full p-2 bg-white rounded-md border border-black" />
+                </div>
+                <div class="flex flex-col mt-4">
+                    <label for="passwordconfirm" class="text-md ">Konfirmasi Kata Sandi</label>
+                    <input type="password" id="passwordconfirm" class="mt-2 w-full p-2 bg-white rounded-md border border-black" />
+                </div>
+                <div class="flex flex-col mt-6">
+                    <button class="text-center w-full p-2 rounded-lg bg-blue-600 text-white">Lanjut</button>
+                    <p class="mt-3 text-center pb-3">Sudah punya akun? <span class="underline text-blue-500 cursor-pointer">Masuk</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
+
 
 <!-- Footer -->
 
