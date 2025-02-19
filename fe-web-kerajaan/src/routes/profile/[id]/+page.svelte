177b0c1eb5d1be2bb5ee4c44006c3ba1f64de33d
Fortibo <pootisspy931@gmail.com>
2025-02-19 14:31:19 +0700

    fixing : detail

--- fe-web-kerajaan/src/routes/profile/[id]/+page.svelte
+++ fe-web-kerajaan/src/routes/profile/[id]/+page.svelte
@@ -106,6 +106,29 @@
 			Simpan Data
 		</button>
 	</div>
+
+	<!-- Tombol Kembali -->
+	<div class="flex w-full md:hidden">
+		<button
+			class="flex w-full items-center justify-center gap-2 rounded-lg bg-red-500 px-4 py-2 text-white hover:cursor-pointer md:w-auto"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+				/>
+			</svg>
+			Kembali
+		</button>
+	</div>
 </div>
 
 <!-- jujur edit profile harus e isa dijaiin komponen karena ngulang terus, tp ak males :D -->
