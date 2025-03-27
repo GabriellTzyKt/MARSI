8112ee2a1318d9ca8286aff76138557e2412bc73
Fortibo <pootisspy931@gmail.com>
2025-03-27 08:55:19 +0700

    t

--- fe-web-kerajaan/src/routes/abdi/sekretariat/anggota/daftaranggota/tambah/+page.svelte
+++ fe-web-kerajaan/src/routes/abdi/sekretariat/anggota/daftaranggota/tambah/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { goto } from '$app/navigation';
 	import SuccessModal from '$lib/modal/SuccessModal.svelte';
 	import { fade } from 'svelte/transition';
+	// import { load } from '../../../../dashboard/organisasi/daftarorganisasi/proxy+page.server';
 
 	let open = $state(false);
 	let timer: number;
+	let rAyah = $state('');
+	let rIbu = $state('');
+	let errors = $state();
+	let loading = $state(false);
 	function setTimer() {
 		open = true;
 		if (timer) {
@@ -19,226 +25,377 @@
 </script>
 
 <div class="min-h-full w-full">
-	<div class="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
-		<!-- 1 -->
-		<div>
+	<form
+		action="?/tambahAbdi"
+		method="post"
+		use:enhance={() => {
+			loading = true;
+			return async ({ result }) => {
+				loading = false;
+				if (result.type === 'success') {
+					clearTimeout(timer);
+					open = true;
+					timer = setTimeout(() => {
+						open = false;
+						goto('/abdi/sekretariat/anggota/daftaranggota');
+					}, 3000);
+				}
+				if (result.type === 'failure') {
+					errors = result.data?.errors;
+				}
+			};
+		}}
+	>
+		<div class="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
+			<!-- 1 -->
 			<div>
-				<p>Nama Lengkap / Asma Timur*:</p>
-				<div class="relative">
-					<input
-						type="text"
-						name="nama_lengkap"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
-				</div>
-			</div>
-
-			<div class="mt-5 flex gap-12">
-				<div class="w-full">
-					<p>Tempat Lahir :</p>
-					<input
-						type="text"
-						name="tempat_lahir"
-						placeholder="Masukkan nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
-					/>
-				</div>
-				<div class="w-full">
-					<p>Tanggal Lahir :</p>
-					<input
-						type="date"
-						name="tanggal_lahir"
-						placeholder="Masukkan nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
-					/>
-				</div>
-				<!-- <span class="bx--calendar"></span> -->
-			</div>
-
-			<div>
-				<p class="mt-5">Alamat:</p>
-				<div class="relative">
-					<input
-						type="text"
-						name="alamat"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
-				</div>
-			</div>
-
-			<div class="mt-5 flex gap-12">
-				<div class="w-full">
-					<p>No telepon :</p>
-					<input
-						type="phone"
-						name="no_telp"
-						placeholder="Masukkan nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
-					/>
-				</div>
-				<div class="w-full">
-					<p>Jenis Kelamin :</p>
-					<select
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
-						name="jenis_kelamin"
-						value="Laki-Laki"
-					>
-						<option value="Laki-Laki">Laki-Laki</option>
-						<option value="Perempuan">Perempuan</option>
-					</select>
-				</div>
-			</div>
-
-			<div>
-				<p class="mt-5">Hobi (Opsional):</p>
-				<div class="relative">
-					<input
-						type="text"
-						name="hobi"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
-				</div>
-			</div>
-		</div>
-
-		<!-- 2 -->
-		<div>
-			<div>
-				<div class="flex justify-between">
-					<p>(Nama Ayah)</p>
-					<p>Ayah seorang abdi?</p>
-					<div class="flex">
-						<div class="mx-2 flex items-center justify-center">
-							<input
-								id="radio-ayah-ya"
-								type="radio"
-								name="radio-ayah"
-								class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
-							/>
-							<label for="radio-ayah-ya" class="mx-2 text-sm font-medium text-gray-900">Ya</label>
-						</div>
-						<div class="mx-2 flex items-center justify-center">
-							<input
-								id="radio-ayah-tidak"
-								type="radio"
-								name="radio-ayah"
-								class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
-								checked
-							/>
-							<label for="radio-ayah-tidak" class="mx-2 text-sm font-medium text-black">Tidak</label
-							>
-						</div>
-					</div>
-				</div>
-				<div class="relative">
-					<input
-						type="text"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
-				</div>
-			</div>
-
-			<div>
-				<div class="mt-5 flex justify-between">
-					<p>(Nama Ibu)</p>
-					<p>Ibu seorang abdi?</p>
-					<div class="flex">
-						<div class="mx-2 flex items-center justify-center">
-							<input
-								id="radio-ibu-ya"
-								type="radio"
-								name="radio-ibu"
-								class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
-							/>
-							<label for="radio-ibu-ya" class="mx-2 text-sm font-medium text-gray-900">Ya</label>
-						</div>
-						<div class="mx-2 flex items-center justify-center">
-							<input
-								id="radio-ibu-tidak"
-								type="radio"
-								name="radio-ibu"
-								class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
-								checked
-							/>
-							<label for="radio-ibu-tidak" class="mx-2 text-sm font-medium text-black">Tidak</label>
-						</div>
-					</div>
-				</div>
-				<div class="relative">
-					<input
-						type="text"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
-				</div>
-			</div>
-
-			<div class="mt-5">
-				<p>Email(Opsional):</p>
-				<div class="relative">
-					<input
-						type="text"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
-				</div>
-			</div>
-
-			<div class="flex gap-2">
-				<div class="mt-5 w-[50%]">
-					<p>Wongso:</p>
+				<div>
+					<p>Nama Lengkap / Asma Timur*:</p>
 					<div class="relative">
 						<input
 							type="text"
+							name="nama_lengkap"
 							placeholder="Masukkan Nama"
 							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
 						/>
 						<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
 					</div>
+					<div>
+						{#if errors}
+							{#each errors.nama_lengkap as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
 				</div>
 
-				<div class="mt-5 w-[50%]">
-					<p>Pekerjaan(Opsional):</p>
+				<div class="mt-5 flex gap-12">
+					<div class="w-full">
+						<p>Tempat Lahir :</p>
+						<input
+							type="text"
+							name="tempat_lahir"
+							placeholder="Masukkan nama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
+						/>
+						<div>
+							{#if errors}
+								{#each errors.tempat_lahir as e}
+									<p class="text-left text-red-500">-{e}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+					<div class="w-full">
+						<p>Tanggal Lahir :</p>
+						<input
+							type="date"
+							name="tanggal_lahir"
+							placeholder="Masukkan nama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
+						/>
+						<div>
+							{#if errors}
+								{#each errors.tanggal_lahir as e}
+									<p class="text-left text-red-500">-{e}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+					<!-- <span class="bx--calendar"></span> -->
+				</div>
+
+				<div>
+					<p class="mt-5">Alamat:</p>
 					<div class="relative">
 						<input
 							type="text"
+							name="alamat"
 							placeholder="Masukkan Nama"
 							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
 						/>
 						<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
 					</div>
+					<div>
+						{#if errors}
+							{#each errors.alamat as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
+				</div>
+
+				<div class="mt-5 flex gap-12">
+					<div class="w-full">
+						<p>No telepon :</p>
+						<input
+							type="phone"
+							name="no_telp"
+							placeholder="Masukkan nama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
+						/>
+						<div>
+							{#if errors}
+								{#each errors.no_telp as e}
+									<p class="text-left text-red-500">-{e}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+					<div class="w-full">
+						<p>Jenis Kelamin :</p>
+						<select
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2 text-start"
+							name="jenis_kelamin"
+							value="Laki-Laki"
+						>
+							<option value="Laki-Laki">Laki-Laki</option>
+							<option value="Perempuan">Perempuan</option>
+						</select>
+						<div>
+							{#if errors}
+								{#each errors.jenis_kelamin as e}
+									<p class="text-left text-red-500">-{e}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+				</div>
+
+				<div>
+					<p class="mt-5">Hobi (Opsional):</p>
+					<div class="relative">
+						<input
+							type="text"
+							name="hobi"
+							placeholder="Masukkan Nama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+						/>
+						<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+					</div>
+					<div>
+						{#if errors}
+							{#each errors.hobi as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
 				</div>
 			</div>
 
-			<div class="mt-5">
-				<p>Agama(Opsional):</p>
-				<div class="relative w-full lg:w-[50%]">
-					<input
-						type="text"
-						placeholder="Masukkan Nama"
-						class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
-					/>
-					<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+			<!-- 2 -->
+			<div>
+				<div>
+					<div class="flex justify-between">
+						<p>(Nama Ayah)</p>
+						<p>Ayah seorang abdi?</p>
+						<div class="flex">
+							<div class="mx-2 flex items-center justify-center">
+								<input
+									id="radio_ayah-ya"
+									type="radio"
+									bind:group={rAyah}
+									value="ayah_ya"
+									name="radioayah"
+									class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
+								/>
+								<label for="radio_ayah-ya" class="mx-2 text-sm font-medium text-gray-900">Ya</label>
+							</div>
+							<div class="mx-2 flex items-center justify-center">
+								<input
+									id="radio_ayah-tidak"
+									type="radio"
+									bind:group={rAyah}
+									value="ayah_ba"
+									name="radioayah"
+									class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
+								/>
+								<label for="radio_ayah-tidak" class="mx-2 text-sm font-medium text-black"
+									>Tidak</label
+								>
+							</div>
+							<div>
+								{#if errors}
+									{#each errors.radio_ayah as e}
+										<p class="text-left text-red-500">-{e}</p>
+									{/each}
+								{/if}
+							</div>
+						</div>
+					</div>
+					<div class="relative">
+						<input
+							type="text"
+							placeholder="Masukkan Nama"
+							name="nama_ayah"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+						/>
+						<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+					</div>
+					<div>
+						{#if errors}
+							{#each errors.nama_ayah as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
+				</div>
+
+				<div>
+					<div class="mt-5 flex justify-between">
+						<p>(Nama Ibu)</p>
+						<p>Ibu seorang abdi?</p>
+						<div class="flex">
+							<div class="mx-2 flex items-center justify-center">
+								<input
+									id="radio_ibu-ya"
+									type="radio"
+									bind:group={rIbu}
+									name="radioibu"
+									value="ibu_ya"
+									class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
+								/>
+								<label for="radio_ibu-ya" class="mx-2 text-sm font-medium text-gray-900">Ya</label>
+							</div>
+							<div class="mx-2 flex items-center justify-center">
+								<input
+									id="radio_ibu-tidak"
+									type="radio"
+									value="ibu_ba"
+									bind:group={rIbu}
+									name="radioibu"
+									class="h-4 w-4 text-blue-600 focus:ring-2 focus:ring-blue-500"
+								/>
+								<label for="radio_ibu-tidak" class="mx-2 text-sm font-medium text-black"
+									>Tidak</label
+								>
+							</div>
+							<div>
+								{#if errors}
+									{#each errors.radio_ibu as e}
+										<p class="text-left text-red-500">-{e}</p>
+									{/each}
+								{/if}
+							</div>
+						</div>
+					</div>
+					<div class="relative">
+						<input
+							type="text"
+							name="nama_ibu"
+							placeholder="Masukkan Nama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+						/>
+						<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+					</div>
+					<div>
+						{#if errors}
+							{#each errors.nama_ibu as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
+				</div>
+
+				<div class="mt-5">
+					<p>Email(Opsional):</p>
+					<div class="relative">
+						<input
+							type="email"
+							name="email"
+							placeholder="Masukkan Nama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+						/>
+						<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+					</div>
+					<div>
+						{#if errors}
+							{#each errors.email as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
+				</div>
+
+				<div class="flex gap-2">
+					<div class="mt-5 w-[50%]">
+						<p>Wongso:</p>
+						<div class="relative">
+							<input
+								type="text"
+								name="wongso"
+								placeholder="Masukkan Wongso"
+								class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+							/>
+							<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+						</div>
+						<div>
+							{#if errors}
+								{#each errors.wongso as e}
+									<p class="text-left text-red-500">-{e}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+
+					<div class="mt-5 w-[50%]">
+						<p>Pekerjaan(Opsional):</p>
+						<div class="relative">
+							<input
+								type="text"
+								placeholder="Masukkan Pekerjaan"
+								name="pekerjaan"
+								class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+							/>
+							<span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span>
+						</div>
+						<div>
+							{#if errors}
+								{#each errors.pekerjaan as e}
+									<p class="text-left text-red-500">-{e}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+				</div>
+
+				<div class="mt-5">
+					<p>Agama(Opsional):</p>
+					<div class="relative w-full lg:w-[50%]">
+						<select
+							placeholder="Masukkan Agama"
+							name="agama"
+							class="mt-2 w-full rounded-lg border-2 border-black px-2 py-2"
+						>
+							<option value="Islam">Islam</option>
+							<option value="Kristen">Kristen</option>
+							<option value="Katolik">Katolik</option>
+							<option value="Hindu">Hindu</option>
+							<option value="Buddha">Buddha</option>
+							<option value="Kong Hu Cu">Kong Hu Cu</option>
+							<option value="Prefer Not To Say">Prefer Not To Say</option>
+						</select>
+						<!-- <span class="raphael--edit absolute right-2 top-1 mt-2.5 opacity-45"></span> -->
+					</div>
+					<div>
+						{#if errors}
+							{#each errors.agama as e}
+								<p class="text-left text-red-500">-{e}</p>
+							{/each}
+						{/if}
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="flex w-full justify-end">
-		<button
-			class="w-50 t-0 mt-10 rounded-lg border-2 border-black bg-green-500 px-2 py-2 text-white"
-			onclick={setTimer}>Tambah Data</button
-		>
-	</div>
+		<div class="flex w-full justify-end">
+			<button
+				class="w-50 t-0 mt-10 rounded-lg border-2 border-black bg-green-500 px-2 py-2 {loading
+					? 'disabled'
+					: ''} text-white"
+				type="submit">{loading ? 'Loading...' : '+Tambah Data'}</button
+			>
+		</div>
+	</form>
 </div>
 {#if open}
 	<div in:fade={{ duration: 100 }} out:fade={{ duration: 100 }}>
